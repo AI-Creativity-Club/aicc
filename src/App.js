750799692a8364ca06ce08aebe8093b8282d5eef
ComfyBenjo <benreed447@gmail.com>
2023-04-02 17:17:33 -0700

    Changed for AICC page

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>AICC webpage under contruction...</code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/AI-Creativity-Club"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Our Github
         </a>
       </header>
     </div>
